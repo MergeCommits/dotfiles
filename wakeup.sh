bca5473bb66427156dab847938a1d5d1951f6a37
Mark Jarjour <21148093+MergeCommits@users.noreply.github.com>
2021-08-23 23:28:30 -0400

    Create wakeup.sh

---
+++ wakeup.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+installOrUpdateHomebrew() {
+    which brew > /dev/null 2>&1
+    if [ $? -eq 1 ]; then
+	    xcode-select --install
+	    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+	    brew update
+    fi
+}
+
+installOrUpdateOhMyZsh() {
+    OMZDIR="$HOME/.oh-my-zsh"
+    if [ ! -d "$OMZDIR" ]; then
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    else
+        upgrade_oh_my_zsh
+    fi
+}
