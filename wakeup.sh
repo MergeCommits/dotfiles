5847458fd60d5cd48679027d18f1ee2179d39395
Mark Jarjour <21148093+MergeCommits@users.noreply.github.com>
2022-01-28 00:16:23 -0500

    Update wakeup.sh

--- wakeup.sh
+++ wakeup.sh
@@ -3,11 +3,13 @@
 installOrUpdateHomebrew() {
     which brew > /dev/null 2>&1
     if [ $? -eq 1 ]; then
-        xcode-select --install
+        xcode-select --install # Assume XCode command line tools are not installed.
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     else
         brew update
     fi
+    
+    brew bundle
 }
 
 installOrUpdateOhMyZsh() {
@@ -18,3 +20,27 @@ installOrUpdateOhMyZsh() {
         upgrade_oh_my_zsh
     fi
 }
+
+textEditSettings() {
+    # Open TextEdit with Plain Text mode
+    defaults write com.apple.TextEdit RichText -int 0
+    # Disable smart quotes
+    defaults write com.apple.TextEdit SmartQuotes -int 0
+}
+
+systemSettings() {
+    defaults write com.apple.screensaver askForPassword -int 1
+    defaults write com.apple.screensaver askForPasswordDelay -int 600
+    
+    # Use list view in all Finder windows by default
+    defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+}
+
+main() {
+    installOrUpdateHomebrew()
+    installOrUpdateOhMyZsh()
+    textEditSettings()
+    systemSettings()
+}
+
+main()
