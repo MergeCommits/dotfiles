165006a4c52d1d3d4020b73b24ed99804550410f
Mark Jarjour <markjarjour@gmail.com>
2023-02-12 16:42:04 -0500

    Added readme. Added quotes around defaults.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # dotfiles
 Original idea.
+
+## Commands
+
+Install all available macOS updates:
+
+    softwareupdate -i -a
+
+Install Xcode Command Line Tools:
+
+    xcode-select --install
+
+Clone the repository in your home directory:
+
+    git clone https://github.com/MergeCommits/dotfiles.git
+
+Install/update the dotfiles, macOS defaults, Homebrew and its packages:
+
+    ./install.sh
--- wakeup.sh
+++ wakeup.sh
@@ -15,41 +15,41 @@ installOrUpdateHomebrew() {
 
 textEditSettings() {
     # Open TextEdit with Plain Text mode
-    defaults write com.apple.TextEdit RichText -int 0
+    defaults write com.apple.TextEdit "RichText" -int 0
     # Disable smart quotes
-    defaults write com.apple.TextEdit SmartQuotes -int 0
+    defaults write com.apple.TextEdit "SmartQuotes" -int 0
 }
 
 systemSettings() {
-    defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
-    defaults write com.apple.finder AppleShowAllFiles -bool true
-    defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+    defaults write com.apple.finder "ShowHardDrivesOnDesktop" -bool "false"
+    defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
+    defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
     
     # When performing a search, search the current folder by default
-    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+    defaults write com.apple.finder "FXDefaultSearchScope" -string "SCcf"
 
     # Disable the warning when changing a file extension
-    defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+    defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false"
 
     # Use column view in all Finder windows by default
-    defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
+    defaults write com.apple.finder "FXPreferredViewStyle" -string "clmv"
 
-    defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+    defaults write NSGlobalDomain "com.apple.swipescrolldirection" -bool "false"
 
     # Disable automatic capitalization.
-    defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+    defaults write NSGlobalDomain "NSAutomaticCapitalizationEnabled" -bool "false"
 
     # Disable smart dashes.
-    defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+    defaults write NSGlobalDomain "NSAutomaticDashSubstitutionEnabled" -bool "false"
 
     # Disable automatic period substitution.
-    defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+    defaults write NSGlobalDomain "NSAutomaticPeriodSubstitutionEnabled" -bool "false"
 
     # Disable smart quotes.
-    defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+    defaults write NSGlobalDomain "NSAutomaticQuoteSubstitutionEnabled" -bool "false"
 
     # Disable auto-correct
-    defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+    defaults write NSGlobalDomain "NSAutomaticSpellingCorrectionEnabled" -bool "false"
 
     killall Finder
 }
