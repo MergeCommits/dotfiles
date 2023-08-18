26ca828e08cf9554a446e72270452c6ce4e7eb0a
Mark Jarjour <markjarjour@gmail.com>
2023-08-18 15:12:35 -0400

    Enable path bar in Finder

--- wakeup.sh
+++ wakeup.sh
@@ -26,6 +26,7 @@ defaultEdits() {
 
     ### Finder
     defaults write com.apple.finder "ShowHardDrivesOnDesktop" -bool "false"
+    defaults write com.apple.finder "ShowPathbar" -bool "true"
     defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
     defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false"
     
